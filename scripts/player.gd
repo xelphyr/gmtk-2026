7bb206f4582327d7f34d47cef2f3a182028be6bf
xelphyr <xelphyrnull@gmail.com>
2026-07-23 18:33:29 +0530

    finished very basic player movememt, added timer UI

---
+++ scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
